8316a13da4ece802080c58e6eee5c9641f181fc8
LakSK98 <lakshitha199809@gmail.com>
2022-08-03 23:33:52 +0530

    Initial Commit

---
+++ Src/Screens/ChatsScreen.js
@@ -0,0 +1,126 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useCallback, useEffect} from 'react';
+import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
+import {LocalNotification} from '../Services/LocalPushController';
+import notifee, {EventType} from '@notifee/react-native';
+import {AndroidColor} from '@notifee/react-native';
+
+function modelMessages(data){
+  {
+    let messages = [];
+    data.map((message,index)=>{
+      messages.push({
+        _id: index,
+        text: message.content,
+        createdAt: message.createdAt,
+        user: {
+          _id: message.sender._id,
+          name: message.sender.name,
+          avatar: 'https://placeimg.com/140/140/any',
+        }}
+        );
+    });
+    return messages;
+  }
+};
+
+export default function ChatsScreen({route}) {
+  const [messages, setMessages] = useState([]);
+
+  async function onDisplayNotification() {
+    await notifee.requestPermission();
+
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
+  useEffect(()=>{
+    fetch(`http://10.0.2.2:8000/api/message/${route.params.chatId}`,{ headers : {Authorization:'Bearer token'}})
+      .then((res)=>{
+        setMessages(modelMessages(res.data));
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+  },[]);
+
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //       _id: 1,
+  //       text: 'Hello developer',
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 2,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //     {
+  //       _id: 2,
+  //       text: 'Hello',
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 1,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //   ]);
+  // }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+    onDisplayNotification();
+  }, []);
+
+  const renderBubble = props => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#0EA35D',
+          },
+        }}
+      />
+    );
+  };
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+      renderBubble={renderBubble}
+      alwaysShowSend={true}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Src/Screens/MassagesScreen.js
@@ -0,0 +1,151 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button, StyleSheet, FlatList} from 'react-native';
+import notifee from '@notifee/react-native';
+import {Searchbar} from 'react-native-paper';
+
+import {
+  Container,
+  Card,
+  UserInfo,
+  UserImgWrapper,
+  UserImg,
+  UserInfoText,
+  UserName,
+  PostTime,
+  MessageText,
+  TextSection,
+} from '../styles/MessageStyles';
+
+// const Messages = [
+//   {
+//     id: '1',
+//     userName: 'Aleena Mitchel',
+//     userImg: require('../Assets/img01.png'),
+//     messageTime: '10 mins ago',
+//     messageText: 'Hello !',
+//   },
+//   {
+//     id: '2',
+//     userName: 'John Smith',
+//     userImg: require('../Assets/img02.png'),
+//     messageTime: '2 hours ago',
+//     messageText: 'what about the project evaluation?',
+//   },
+//   {
+//     id: '3',
+//     userName: 'Ken William',
+//     userImg: require('../Assets/img01.png'),
+//     messageTime: '1 hours ago',
+//     messageText: '🤩✌️',
+//   },
+// ];
+
+function modelChats(data){
+  let chats = [];
+  data.map((chat,index)=>{
+    chats.push({
+      id: index,
+      _id: chat._id,
+      userName: chat.chatName,
+      userImg: require('../Assets/img01.png'),
+      messageTime: latestMessage.createdAt,
+      messageText: latestMessage.content,
+    });
+  });
+  return chats;
+}
+
+const MessagesScreen = ({navigation}) => {
+
+  const [Messages,setMessages] = useState([]);
+  // const onChangeSearch = searchQuery => {
+  //   if (searchQuery) {
+  //     const formatedData = searchQuery.toLowerCase();
+  //     const filterData = filter(data, creative => {
+  //       return contains(creative, formatedData);
+  //     });
+  //     setData(filterData);
+  //   } else {
+  //     setData(data);
+  //   }
+  // };
+
+  // const contains = ({creativeHeading, creativeDescription}, searchQuery) => {
+  //   if (
+  //     creativeHeading.toLowerCase().includes(searchQuery) ||
+  //     creativeDescription.toLowerCase().includes(searchQuery)
+  //   ) {
+  //     return true;
+  //   }
+  //   return false;
+  // };
+
+  useEffect(()=>{
+    fetch('http://10.0.2.2:8000/api/chat', { headers : {Authorization:'Bearer token'}})
+      .then((res)=>{
+        setMessages(modelChats(res.data));
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+  },[]);
+
+  return (
+    <Container>
+      <View style={styles.searchBarContainer}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={searchQuery => onChangeSearch(searchQuery)}
+          autoCapitalize="none"
+          style={styles.searchBar}
+        />
+      </View>
+      <FlatList
+        data={Messages}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <Card
+            onPress={() =>
+              navigation.navigate('Chat', {chatId: item._id, chatName: item.userName})
+            }>
+            <UserInfo>
+              <UserImgWrapper>
+                <UserImg source={item.userImg} />
+              </UserImgWrapper>
+              <TextSection>
+                <UserInfoText>
+                  <UserName>{item.userName}</UserName>
+                  <PostTime>{item.messageTime}</PostTime>
+                </UserInfoText>
+                <MessageText>{item.messageText}</MessageText>
+              </TextSection>
+            </UserInfo>
+          </Card>
+        )}
+      />
+    </Container>
+  );
+};
+
+export default MessagesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    padding: 0,
+    margin: 0,
+  },
+  searchBar: {
+    flex: 1,
+    marginVertical: 10,
+    height: 45,
+    fontSize: 18,
+  },
+});
