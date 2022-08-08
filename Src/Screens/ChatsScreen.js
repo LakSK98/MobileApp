653f8108b59ea6df3a8cea9f5d5cf75bb113471f
LakSK98 <93567846+LakSK98@users.noreply.github.com>
2022-08-09 00:04:33 +0530

    Update ChatsScreen.js
    Updated

--- Src/Screens/ChatsScreen.js
+++ Src/Screens/ChatsScreen.js
@@ -50,7 +50,7 @@ export default function ChatsScreen({route}) {
   }
 
   const getToken = async()=>{
-    return await fetch(`http://10.0.2.2:8000/api/login/`,{
+    return await fetch(`http://10.0.2.2:8000/api/user/login/`,{
       method:"POST",
       headers:{
         'Content-Type':'application/json'
